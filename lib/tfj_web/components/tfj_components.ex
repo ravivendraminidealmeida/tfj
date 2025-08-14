67a7bd91cfa1d276c4acb710d5c862a24725d6e8
RaviVendraminideAlmeida <ravivendramini@gmail.com>
2025-08-14 14:28:53 -0300

    feat: header :)

---
+++ lib/tfj_web/components/tfj_components.ex
@@ -0,0 +1,48 @@
+defmodule TfjComponents do
+  use Phoenix.Component
+  use Gettext, backend: TfjWeb.Gettext
+
+  @doc """
+  Cell-shaded button built by me :)
+
+  ## Examples
+  <.button>Action</.button>
+  """
+  attr :rest, :global, include: ~w(href navigate patch method download name value disabled)
+  attr :class, :string
+  attr :variant, :string, values: ~w(primary)
+  slot :inner_block, required: true
+
+  def button(%{rest: rest} = assigns) do
+    variants = %{
+      "danger" => "border-1 border-red-500 bg-red-200 p-1  text-red-500",
+      nil => "border-1 p-1"
+    }
+
+    assigns =
+      assign_new(assigns, :class, fn ->
+        [Map.fetch!(variants, assigns[:variant])]
+      end)
+
+    if rest[:href] || rest[:navigate] || rest[:patch] do
+      ~H"""
+      <.link class={@class} {@rest}>{render_slot(@inner_block)}</.link>
+      """
+    else
+      ~H"""
+      <button class={@class} {@rest}>{render_slot(@inner_block)}</button>
+      """
+    end
+  end
+
+  def header(assigns) do
+    ~H"""
+    <header class="w-full p-3 border-b-1 border-red-300 border-slate flex items-center justify-between">
+      <h1 class="text-xl">
+        <span class="text-red-300">t</span>rying to <span class="text-red-300">f</span>ind a <span class="text-red-300">j</span>ob
+      </h1>
+       <TfjComponents.button variant="danger">get me a job!</TfjComponents.button>
+    </header>
+    """
+  end
+end
